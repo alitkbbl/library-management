f8769661c8918e857510375a0d24799e1951e1b2
Ali <alitaghizadeh8305@gmail.com>
2025-01-10 19:24:01 +0330

    Delete dist directory

--- dist/components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-'use client';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Footer;
-function Footer() {
-    return (<div className="bg-slate-900 z-10">
-      <p>footer</p>
-    </div>);
-}
